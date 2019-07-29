e403f780711864ddfc9f1c2740f71228a290ecbf
Chris Gregory <czipperz@gmail.com>
2019-07-28 22:26:26 -0700

    Make tests setting the global allocator cleanup

--- tests/arena.cpp
+++ tests/arena.cpp
@@ -1,9 +1,9 @@
 #include "catch.hpp"
 
+#include "../src/defer.hpp"
 #include "../src/mem.hpp"
 
-using cz::mem::Arena;
-using cz::mem::global_allocator;
+using namespace cz::mem;
 
 TEST_CASE("Arena::alloc fails when no space left") {
     char buffer[1] = {0};
@@ -42,10 +42,7 @@ struct TestRealloc {
     bool called;
 };
 
-void* test_realloc(void* _data,
-                   void* old_ptr,
-                   size_t old_size,
-                   size_t new_size) {
+void* test_realloc(void* _data, void* old_ptr, size_t old_size, size_t new_size) {
     auto data = static_cast<TestRealloc*>(_data);
     REQUIRE(data->expected_old_ptr == old_ptr);
     REQUIRE(data->expected_old_size == old_size);
@@ -58,6 +55,7 @@ TEST_CASE("Arena::sized allocates memory") {
     char buffer[8];
     TestRealloc test = {buffer, NULL, 0, 8, false};
     global_allocator = {test_realloc, &test};
+    CZ_DEFER(global_allocator = heap::allocator());
 
     auto arena = Arena::sized(8);
     REQUIRE(arena.alloc(8) == buffer);
@@ -70,6 +68,7 @@ TEST_CASE("Arena::drop deallocates memory") {
     Arena arena(buffer, 8);
     TestRealloc test = {NULL, buffer, 8, 0, false};
     global_allocator = {test_realloc, &test};
+    CZ_DEFER(global_allocator = heap::allocator());
 
     arena.drop();
 
