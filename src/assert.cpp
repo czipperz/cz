d98cc5021c5ae9274213d5cce0b761015fe0d98f
Chris Gregory <czipperz@gmail.com>
2019-07-28 18:57:02 -0700

    Make CZ_ASSERT failing abort and print to stderr

--- src/assert.cpp
+++ src/assert.cpp
@@ -1,12 +1,14 @@
 #include "assert.hpp"
 
 #include <stdio.h>
+#include <stdlib.h>
 
 namespace cz {
 
 void assert_(SourceLocation loc, const char* expression_string, bool value) {
     if (!value) {
-        printf("%s:%u: Assertion failed: '%s'", loc.file, loc.line, expression_string);
+        fprintf(stderr, "%s:%u: Assertion failed: '%s'", loc.file, loc.line, expression_string);
+        abort();
     }
 }
 
