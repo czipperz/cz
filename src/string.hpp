39ae580a78f776b4a7b75a0e66979bbdef6aad5b
Chris Gregory <czipperz@gmail.com>
2019-07-29 22:05:13 -0700

    Add String documentation

--- src/string.hpp
+++ src/string.hpp
@@ -27,24 +27,36 @@ class String {
     size_t _cap;
 
 public:
+    /// Create a new empty \c String.
     String();
+    /// Create a new \c String using the \c buffer with initial length and capacity \c len.
     String(char* buffer, size_t len);
+    /// Create a new \c String using the \c buffer with initial length \c len and capacity \c cap.
     String(char* buffer, size_t len, size_t cap);
 
+    /// Create a new \c String allocating a new buffer as a copy of the inputted string.
     explicit String(Str str_to_copy);
 
+    /// Ensure there are \c extra bytes available in the buffer.
     void reserve(size_t extra);
+    /// Append the \c Str to the buffer.
     void append(Str str);
 
+    /// Dealloc the \c buffer.
     void drop();
 
+    /// Get the byte buffer backing the string.
     char* buffer();
+    /// Get the byte buffer backing the string.
     const char* buffer() const;
+    /// Get the length of the string in bytes.
     size_t len() const;
+    /// Get the capacity of the string in bytes.
     size_t cap() const;
 
+    /// Get a \c Str representing this \c String in its current state.
     Str as_str() const;
-
+    /// See \c String::as_str().
     operator Str() const { return as_str(); }
 
     bool operator==(const Str& other) const { return this->as_str() == other; }
