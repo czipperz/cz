5c9b7e6615c4cb8de2495545c2b8188de3ba2540
Chris Gregory <czipperz@gmail.com>
2019-07-29 20:42:04 -0700

    Use CHECK with consecutive assertions

--- tests/mock_allocator.cpp
+++ tests/mock_allocator.cpp
@@ -8,8 +8,8 @@ namespace test {
 
 static void* test_realloc(void* _data, void* old_ptr, size_t old_size, size_t new_size) {
     auto data = static_cast<MockAllocator*>(_data);
-    REQUIRE(data->expected_old_ptr == old_ptr);
-    REQUIRE(data->expected_old_size == old_size);
+    CHECK(data->expected_old_ptr == old_ptr);
+    CHECK(data->expected_old_size == old_size);
     REQUIRE(data->expected_new_size == new_size);
     data->called = true;
     return data->buffer;
