3bd1d1d04ccb582a71dc7b7497c75bed8614cf2b
Chris Gregory <czipperz@gmail.com>
2019-07-30 21:41:56 -0700

    Add AllocInfo equality operators and reformat

--- src/mem/alloc_info.hpp
+++ src/mem/alloc_info.hpp
@@ -8,14 +8,16 @@ namespace mem {
 struct AllocInfo {
     size_t size;
     size_t alignment;
+
+    constexpr bool operator==(const AllocInfo& other) const {
+        return size == other.size && alignment == other.alignment;
+    }
+    constexpr bool operator!=(const AllocInfo& other) const { return !(*this == other); }
 };
 
 template <class T>
 AllocInfo alloc_info() {
-    return {
-        sizeof(T),
-        alignof(T)
-    };
+    return {sizeof(T), alignof(T)};
 }
 
 }
