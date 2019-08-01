24cc6289c742bbb38fc3e99eda63b98fd77b82c5
Chris Gregory <czipperz@gmail.com>
2019-07-31 22:03:02 -0700

    Allow slices to be indexed and make MemSlice a separate type.

--- src/slice.hpp
+++ src/slice.hpp
@@ -4,12 +4,25 @@
 
 namespace cz {
 
+/// A slice of an array of the given type.
+///
+/// If you want the inner type to be \c void, instead use \c MemSlice.
 template <class T>
 struct Slice {
     T* buffer;
     size_t len;
+
+    T& operator[](size_t index) const { return buffer[index]; }
 };
 
-using MemSlice = Slice<void>;
+/// A slice of memory with an unspecified type.
+///
+/// Since \c void* cannot be indexed to because the type of the data cannot be
+/// statically determined, \c MemSlice doesn't support as much functionality as
+/// \c Slice.
+struct MemSlice {
+    void* buffer;
+    size_t len;
+};
 
 }
