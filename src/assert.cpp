f00dcea643afc623dfdd81ea09c87b5b4afb629d
Chris Gregory <czipperz@gmail.com>
2019-08-02 21:58:24 -0700

    Use logger rather than printing manually

--- src/assert.cpp
+++ src/assert.cpp
@@ -1,19 +1,20 @@
 #include "assert.hpp"
 
-#include <stdio.h>
 #include <stdlib.h>
+#include "logger.hpp"
 
 namespace cz {
 
 void assert_(SourceLocation loc, const char* expression_string, bool value) {
     if (!value) {
-        fprintf(stderr, "%s:%u: Assertion failed: '%s'", loc.file, loc.line, expression_string);
+        io::write(log::fatal(), loc.file, ':', loc.line, ": Assertion failed: '", expression_string,
+                  '\'');
         abort();
     }
 }
 
 void panic_(SourceLocation loc, const char* message) {
-    fprintf(stderr, "%s:%u: %s", loc.file, loc.line, message);
+    io::write(log::fatal(), loc.file, ':', loc.line, ": ", message);
     abort();
 }
 
