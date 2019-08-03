acb92910d53e101244c3b6744858364cf6c89c5f
Chris Gregory <czipperz@gmail.com>
2019-08-02 22:06:21 -0700

    Print new line on assert failure and panic

--- src/assert.cpp
+++ src/assert.cpp
@@ -8,13 +8,13 @@ namespace cz {
 void assert_(SourceLocation loc, const char* expression_string, bool value) {
     if (!value) {
         io::write(log::fatal(), loc.file, ':', loc.line, ": Assertion failed: '", expression_string,
-                  '\'');
+                  "'\n");
         abort();
     }
 }
 
 void panic_(SourceLocation loc, const char* message) {
-    io::write(log::fatal(), loc.file, ':', loc.line, ": ", message);
+    io::write(log::fatal(), loc.file, ':', loc.line, ": ", message, '\n');
     abort();
 }
 
