16ead2bc22f5ab0289b3e65fa8028ffdb6e29061
Chris Gregory <czipperz@gmail.com>
2019-08-06 22:23:53 -0700

    Use tprint in log() to format the message

--- src/logger.hpp
+++ src/logger.hpp
@@ -10,8 +10,9 @@ namespace log {
 
 template <LogLevel level, class... Ts>
 void log(C* c, Ts... ts) {
-    String message = cz::format::sprint(c, ts...);
-    CZ_DEFER(message.drop(c));
+    String message = cz::format::tprint(c, ts...);
+    message.trealloc(c);
+    CZ_DEFER(message.tdrop(c));
     c->log(LogInfo(level, message));
 }
 
