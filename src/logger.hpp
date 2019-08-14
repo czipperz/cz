dc35f92095bc070cd77d98207deececd38de8982
Chris Gregory <czipperz@gmail.com>
2019-08-13 19:49:48 -0700

    Allow logging with dynamic log level

--- src/logger.hpp
+++ src/logger.hpp
@@ -8,16 +8,16 @@
 namespace cz {
 namespace log {
 
-template <LogLevel level, class... Ts>
-void log(C* c, const char* file, size_t line, Ts... ts) {
+template <class... Ts>
+void log(C* c, LogLevel level, const char* file, size_t line, Ts... ts) {
     String message = cz::format::sprint(c->temp, ts...);
     message.realloc(c->temp);
     CZ_DEFER(message.drop(c->temp));
     c->log(LogInfo(file, line, level, message));
 }
 
-#define CZ_LOG(c, level, ...) \
-    (cz::log::log<cz::log::LogLevel::level>(c, __FILE__, __LINE__, __VA_ARGS__))
+#define CZ_LOG(c, level, ...) (CZ_LOGL(c, cz::log::LogLevel::level, __VA_ARGS__))
+#define CZ_LOGL(c, level, ...) (cz::log::log(c, level, __FILE__, __LINE__, __VA_ARGS__))
 
 Logger ignore();
 
