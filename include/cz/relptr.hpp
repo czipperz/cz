221deb3314d98bd2d06a4d345cd49d34526dacf1
Chris Gregory <czipperz@gmail.com>
2019-08-30 09:07:04 -0700

    Add RelPtr::set

--- include/cz/relptr.hpp
+++ include/cz/relptr.hpp
@@ -6,6 +6,8 @@ template <class T, class I>
 struct RelPtr {
     I offset;
 
+    void set(T* t) { offset = reinterpret_cast<const char*>(t) - reinterpret_cast<char*>(this); }
+
     constexpr T* get() const {
         return const_cast<T*>(
             reinterpret_cast<const T*>(reinterpret_cast<const char*>(this) + offset));
