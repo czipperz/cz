e01944013dc02528ea77f7ae287f06a3b540d654
Chris Gregory <czipperz@gmail.com>
2019-09-07 20:31:26 -0700

    Use aligned heap allocation when possible

--- src/mem/heap.cpp
+++ src/mem/heap.cpp
@@ -1,13 +1,34 @@
 #include <cz/mem/heap.hpp>
 
+#if defined(_WIN32) || _ISOC11_SOURCE
+#include <malloc.h>
+#else
 #include <stdlib.h>
+#endif
+
+#include <cz/assert.hpp>
+#include <cz/util.hpp>
 
 namespace cz {
 namespace mem {
 
 static MemSlice heap_alloc(void*, AllocInfo info) {
-    // TODO alignment
-    auto buf = malloc(info.size);
+#ifdef _WIN32
+    void* buf = _aligned_malloc(info.size, info.alignment);
+#elif _ISOC11_SOURCE || __cplusplus >= 201703L
+    void* buf = aligned_alloc(info.alignment, info.size);
+#elif _POSIX_C_SOURCE >= 200112L || _XOPEN_SOURCE >= 600
+    void* buf;
+    if (posix_memalign(&buf, max(info.alignment, alignof(void*)), info.size) == 0) {
+        return {buf, info.size};
+    } else {
+        return {nullptr, 0};
+    }
+#else
+    CZ_DEBUG_ASSERT(info.alignment <= alignof(max_align_t));
+    void* buf = malloc(info.size);
+#endif
+
     if (buf) {
         return {buf, info.size};
     } else {
@@ -16,12 +37,19 @@ static MemSlice heap_alloc(void*, AllocInfo info) {
 }
 
 static void heap_dealloc(void*, MemSlice mem) {
+#ifdef _WIN32
+    _aligned_free(mem.buffer);
+#else
     free(mem.buffer);
+#endif
 }
 
 static MemSlice heap_realloc(void*, MemSlice old_mem, AllocInfo new_info) {
-    // TODO alignment
-    auto buf = realloc(old_mem.buffer, new_info.size);
+#ifdef _WIN32
+    void* buf = _aligned_realloc(old_mem.buffer, new_info.size, new_info.alignment);
+#else
+    void* buf = realloc(old_mem.buffer, new_info.size);
+#endif
     if (buf) {
         return {buf, new_info.size};
     } else {
