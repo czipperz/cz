182a847f7bebfdfa027cfc5b1a3d3749304b9ff6
Chris Gregory <czipperz@gmail.com>
2019-09-07 21:28:01 -0700

    Test RelPtr set and copy

--- tests/relptr.cpp
+++ tests/relptr.cpp
@@ -6,6 +6,23 @@ struct S {
     char bytes[8];
 };
 
+TEST_CASE("RelPtr set") {
+    cz::RelPtr<S, int> ptr;
+    S s;
+    ptr.set(&s);
+    REQUIRE(ptr.get() == &s);
+}
+
+TEST_CASE("RelPtr copy is bitwise") {
+    struct {
+        cz::RelPtr<S, int> ptr;
+        S s;
+    } array[2];
+    array[0].ptr.set(&array[0].s);
+    array[1].ptr = array[0].ptr;
+    REQUIRE(array[1].ptr.get() == &array[1].s);
+}
+
 TEST_CASE("RelPtr get") {
     cz::RelPtr<S, int> ptr = {15};
     REQUIRE(ptr.get() == (S*)((char*)&ptr + 15));
