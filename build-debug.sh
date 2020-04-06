fbc2830be19ba428b58e7638a113ea6ea985b79d
Chris Gregory <czipperz@gmail.com>
2020-04-06 01:25:26 -0400

    Build tests with ./build-debug.sh

--- build-debug.sh
+++ build-debug.sh
@@ -6,7 +6,7 @@ cd "$(dirname "$0")"
 mkdir -p build/debug
 
 (cd build/debug
- cmake -DCMAKE_BUILD_TYPE=Debug ../.. >/dev/null
+ cmake -DCMAKE_BUILD_TYPE=Debug -DCZ_BUILD_TESTS=1 ../.. >/dev/null
  cmake --build .)
 
 ./run-tests.sh
