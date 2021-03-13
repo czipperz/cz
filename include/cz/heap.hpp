da3b72abf93ae4ae28ed6fba9c2458edd0418529
Czipperz <czipperz@gmail.com>
2021-03-13 00:00:43 -0800

    Expose heap_allocator implementation details so they can be easily optimized

--- include/cz/heap.hpp
+++ include/cz/heap.hpp
@@ -1,10 +1,29 @@
 #pragma once
 
+#include <stdlib.h>
+#include "assert.hpp"
 #include "allocator.hpp"
 
 namespace cz {
 
+inline void* heap_allocator_alloc(void*, AllocInfo info) {
+    CZ_DEBUG_ASSERT(info.alignment <= alignof(max_align_t));
+    return malloc(info.size);
+}
+
+inline void heap_allocator_dealloc(void*, MemSlice mem) {
+    free(mem.buffer);
+}
+
+inline void* heap_allocator_realloc(void*, MemSlice old_mem, AllocInfo new_info) {
+    return realloc(old_mem.buffer, new_info.size);
+}
+
 /// Make an allocator that allocates memory in the heap.
-Allocator heap_allocator();
+inline Allocator heap_allocator() {
+    static const Allocator::VTable vtable = {heap_allocator_alloc, heap_allocator_dealloc,
+                                             heap_allocator_realloc};
+    return {&vtable, nullptr};
+}
 
 }
