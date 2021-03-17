8086dcf0b8db7b24b01d58fe98eefc834d78b674
Chris Gregory <czipperz@gmail.com>
2021-03-16 23:46:01 -0500

    Handle sizeof({HANDLE,sem_t,pthread_mutex_t})!=void*

--- src/mutex.cpp
+++ src/mutex.cpp
@@ -1,5 +1,6 @@
 #include <cz/mutex.hpp>
 
+#include <stdlib.h>
 #include <cz/assert.hpp>
 
 #ifdef _WIN32
@@ -12,46 +13,80 @@
 
 namespace cz {
 
+// Convert a void* to the primitive type.
 #ifdef _WIN32
-static_assert(sizeof(HANDLE) == sizeof(void*), "");
+HANDLE h(void*& handle) {
+    if (sizeof(HANDLE) <= sizeof(void*)) {
+        return *(HANDLE*)&handle;
+    } else {
+        return *(HANDLE*)handle;
+    }
+}
 #else
-static_assert(sizeof(pthread_mutex_t) == sizeof(void*), "");
+pthread_mutex_t* h(void*& handle) {
+    if (sizeof(pthread_mutex_t) <= sizeof(void*)) {
+        return *(pthread_mutex_t**)&handle;
+    } else {
+        return (pthread_mutex_t*)handle;
+    }
+}
 #endif
 
 void Mutex::init() {
 #ifdef _WIN32
-    HANDLE h = CreateMutexA(/*security_attributes=*/nullptr, /*start_locked=*/false,
-                            /*name=*/nullptr);
-    handle = *(void**)&h;
+    if (sizeof(HANDLE) > sizeof(void*)) {
+        handle = malloc(sizeof(HANDLE));
+        CZ_ASSERT(handle);
+    }
+
+    HANDLE hh = CreateMutexA(/*security_attributes=*/nullptr, /*start_locked=*/false,
+                             /*name=*/nullptr);
+
+    if (sizeof(HANDLE) <= sizeof(void*)) {
+        *(HANDLE*)&handle = hh;
+    } else {
+        *(HANDLE*)handle = hh;
+    }
 #else
-    pthread_mutex_init((pthread_mutex_t*)&handle, /*attr=*/nullptr);
+    if (sizeof(pthread_mutex_t) > sizeof(void*)) {
+        handle = malloc(sizeof(pthread_mutex_t));
+        CZ_ASSERT(handle);
+    }
+
+    pthread_mutex_init(h(handle), /*attr=*/nullptr);
 #endif
 }
 
 void Mutex::drop() {
 #ifdef _WIN32
-    CloseHandle(*(HANDLE*)&handle);
+    CloseHandle(h(handle));
+    if (sizeof(HANDLE) > sizeof(void*)) {
+        free(handle);
+    }
 #else
-    pthread_mutex_destroy((pthread_mutex_t*)&handle);
+    pthread_mutex_destroy(h(handle));
+    if (sizeof(pthread_mutex_t) > sizeof(void*)) {
+        free(handle);
+    }
 #endif
 }
 
 void Mutex::unlock() {
 #ifdef _WIN32
-    DWORD ret = ReleaseMutex(*(HANDLE*)&handle);
+    DWORD ret = ReleaseMutex(h(handle));
     CZ_ASSERT(ret != 0);
 #else
-    int ret = pthread_mutex_unlock((pthread_mutex_t*)&handle);
+    int ret = pthread_mutex_unlock(h(handle));
     CZ_ASSERT(ret == 0);
 #endif
 }
 
 void Mutex::lock() {
 #ifdef _WIN32
-    DWORD ret = WaitForSingleObject(*(HANDLE*)&handle, /*milli_seconds=*/INFINITE);
+    DWORD ret = WaitForSingleObject(h(handle), /*milli_seconds=*/INFINITE);
     CZ_ASSERT(ret == WAIT_OBJECT_0);
 #else
-    int ret = pthread_mutex_lock((pthread_mutex_t*)&handle);
+    int ret = pthread_mutex_lock(h(handle));
     CZ_ASSERT(ret == 0);
 #endif
 }
@@ -59,10 +94,10 @@ void Mutex::lock() {
 bool Mutex::try_lock() {
 #ifdef _WIN32
     // Note that this ignores errors.
-    return WaitForSingleObject(*(HANDLE*)&handle, /*milli_seconds=*/0) == WAIT_OBJECT_0;
+    return WaitForSingleObject(h(handle), /*milli_seconds=*/0) == WAIT_OBJECT_0;
 #else
     // Note that this ignores errors.
-    return pthread_mutex_trylock((pthread_mutex_t*)&handle) == 0;
+    return pthread_mutex_trylock(h(handle)) == 0;
 #endif
 }
 
--- src/semaphore.cpp
+++ src/semaphore.cpp
@@ -1,5 +1,6 @@
 #include <cz/semaphore.hpp>
 
+#include <stdlib.h>
 #include <cz/assert.hpp>
 
 #ifdef _WIN32
@@ -12,47 +13,80 @@
 
 namespace cz {
 
+// Convert a void* to the primitive type.
 #ifdef _WIN32
-static_assert(sizeof(HANDLE) == sizeof(void*), "");
+HANDLE h(void*& handle) {
+    if (sizeof(HANDLE) <= sizeof(void*)) {
+        return *(HANDLE*)&handle;
+    } else {
+        return *(HANDLE*)handle;
+    }
+}
 #else
-static_assert(sizeof(sem_t) == sizeof(void*), "");
+sem_t* h(void*& handle) {
+    if (sizeof(sem_t) <= sizeof(void*)) {
+        return *(sem_t**)&handle;
+    } else {
+        return (sem_t*)handle;
+    }
+}
 #endif
 
 void Semaphore::init(uint32_t initial_value) {
 #ifdef _WIN32
-    HANDLE h = CreateSemaphoreA(/*security_attributes=*/nullptr, initial_value,
-                                /*maximum=*/1 << 15, /*name=*/nullptr);
-    handle = *(void**)&h;
+    if (sizeof(HANDLE) > sizeof(void*)) {
+        handle = malloc(sizeof(HANDLE));
+        CZ_ASSERT(handle);
+    }
+
+    HANDLE hh = CreateSemaphoreA(/*security_attributes=*/nullptr, initial_value,
+                                 /*maximum=*/1 << 15, /*name=*/nullptr);
+
+    if (sizeof(HANDLE) <= sizeof(void*)) {
+        *(HANDLE*)&handle = hh;
+    } else {
+        *(HANDLE*)handle = hh;
+    }
 #else
-    sem_init((sem_t*)&handle, /*is_shared_between_processes=*/0, initial_value);
+    if (sizeof(sem_t) > sizeof(void*)) {
+        handle = malloc(sizeof(sem_t));
+        CZ_ASSERT(handle);
+    }
+
+    sem_init(h(handle), /*is_shared_between_processes=*/0, initial_value);
 #endif
 }
 
 void Semaphore::drop() {
 #ifdef _WIN32
-    CloseHandle(*(HANDLE*)&handle);
+    CloseHandle(h(handle));
+    if (sizeof(HANDLE) > sizeof(void*)) {
+        free(handle);
+    }
 #else
-    sem_destroy((sem_t*)&handle);
+    sem_destroy(h(handle));
+    if (sizeof(sem_t) > sizeof(void*)) {
+        free(handle);
+    }
 #endif
 }
 
 void Semaphore::release() {
 #ifdef _WIN32
-    DWORD ret =
-        ReleaseSemaphore(*(HANDLE*)&handle, /*increment=*/1, /*previous_count_out=*/nullptr);
+    DWORD ret = ReleaseSemaphore(h(handle), /*increment=*/1, /*previous_count_out=*/nullptr);
     CZ_ASSERT(ret != 0);
 #else
-    int ret = sem_post((sem_t*)&handle);
+    int ret = sem_post(h(handle));
     CZ_ASSERT(ret == 0);
 #endif
 }
 
 void Semaphore::acquire() {
 #ifdef _WIN32
-    DWORD ret = WaitForSingleObject(*(HANDLE*)&handle, /*milli_seconds=*/INFINITE);
+    DWORD ret = WaitForSingleObject(h(handle), /*milli_seconds=*/INFINITE);
     CZ_ASSERT(ret == WAIT_OBJECT_0);
 #else
-    int ret = sem_wait((sem_t*)&handle);
+    int ret = sem_wait(h(handle));
     CZ_ASSERT(ret == 0);
 #endif
 }
@@ -60,10 +94,10 @@ void Semaphore::acquire() {
 bool Semaphore::try_acquire() {
 #ifdef _WIN32
     // Note that this ignores errors.
-    return WaitForSingleObject(*(HANDLE*)&handle, /*milli_seconds=*/0) == WAIT_OBJECT_0;
+    return WaitForSingleObject(h(handle), /*milli_seconds=*/0) == WAIT_OBJECT_0;
 #else
     // Note that this ignores errors.
-    return sem_trywait((sem_t*)&handle) == 0;
+    return sem_trywait(h(handle)) == 0;
 #endif
 }
 
