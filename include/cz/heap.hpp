ede2f812d3fc21ec0b9b9948c9541b92f6a15a5b
Czipperz <czipperz@gmail.com>
2021-03-21 15:54:36 -0700

    Instrument heap allocator for tracy

--- include/cz/heap.hpp
+++ include/cz/heap.hpp
@@ -1,23 +1,38 @@
 #pragma once
 
 #include <stdlib.h>
-#include "assert.hpp"
 #include "allocator.hpp"
+#include "assert.hpp"
+
+#ifdef TRACY_ENABLE
+#include <Tracy.hpp>
+#else
+#define TracyAlloc(x, y)
+#define TracyFree(x)
+#endif
 
 namespace cz {
 
 inline void* heap_allocator_alloc(void*, AllocInfo info) {
     CZ_DEBUG_ASSERT(info.alignment <= alignof(max_align_t));
-    return malloc(info.size);
+    void* ptr = malloc(info.size);
+    TracyAlloc(ptr, info.size);
+    return ptr;
 }
 
 inline void heap_allocator_dealloc(void*, MemSlice mem) {
+    TracyFree(mem.buffer);
     free(mem.buffer);
 }
 
 inline void* heap_allocator_realloc(void*, MemSlice old_mem, AllocInfo new_info) {
     CZ_DEBUG_ASSERT(new_info.alignment <= alignof(max_align_t));
-    return realloc(old_mem.buffer, new_info.size);
+    void* ptr = realloc(old_mem.buffer, new_info.size);
+    if (ptr) {
+        TracyFree(old_mem.buffer);
+        TracyAlloc(ptr, new_info.size);
+    }
+    return ptr;
 }
 
 /// Make an allocator that allocates memory in the heap.
