d5b48b57b0dd398e7771ea8198516799da956dfe
Czipperz <czipperz@gmail.com>
2021-03-21 14:53:38 -0700

    Add Allocator::alloc, dealloc for arrays.  Also assert that allocations succeed before trying to use the allocated memory

--- include/cz/allocator.hpp
+++ include/cz/allocator.hpp
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <new>
 #include "alloc_info.hpp"
+#include "assert.hpp"
 #include "slice.hpp"
 
 namespace cz {
@@ -29,10 +30,19 @@ struct Allocator {
         return (T*)alloc(alloc_info<T>());
     }
 
+    /// Allocate memory to store an array of values of the given type using this allocator.
+    template <class T>
+    T* alloc(size_t count) const {
+        AllocInfo info = alloc_info<T>();
+        info.size *= count;
+        return (T*)alloc(info);
+    }
+
     /// Allocate and initialize an object of the given type using this allocator.
     template <class T>
     T* create() const {
         T* obj = alloc<T>();
+        CZ_ASSERT(obj);
         new (obj) T();
         return obj;
     }
@@ -41,12 +51,19 @@ struct Allocator {
     template <class T>
     cz::Slice<T> duplicate(cz::Slice<T> slice) {
         T* new_elems = static_cast<T*>(alloc({sizeof(T) * slice.len, alignof(T)}));
+        CZ_ASSERT(new_elems);
         memcpy(new_elems, slice.elems, sizeof(T) * slice.len);
         return {new_elems, slice.len};
     }
 
     /// Deallocate memory allocated using this allocator.
     void dealloc(MemSlice mem) const { return vtable->dealloc(data, mem); }
+
+    /// Deallocate an array of a given type or a single element if the count isn't passed.
+    template <class T>
+    void dealloc(T* t, size_t count = 1) const {
+        return dealloc({t, sizeof(T) * count});
+    }
 
     /// Reallocate a section of memory allocated using this allocator.
     void* realloc(MemSlice old_mem, AllocInfo new_info) const {
