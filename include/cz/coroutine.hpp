20ace05411e252ed9976d186cc89e36e92fe4f85
Chris Gregory <czipperz@gmail.com>
2021-04-22 22:26:39 -0700

    Document coroutine

--- include/cz/coroutine.hpp
+++ include/cz/coroutine.hpp
@@ -1,5 +1,63 @@
 #pragma once
 
+/// A `Coroutine` is a user-space thread that yields priority at
+/// will.  This is useful for when you need lightweight threads.
+///
+/// Note that under the hood `CZ_CO_START` is a `swtich` statement and `CZ_CO_YIELD` is a `return`
+/// statement.  Using a local variable or `CZ_DEFER` in conjugation with a `Coroutine` is not a good
+/// idea.  Instead store the variables you want to use in the struct you derive from `Coroutine`.
+///
+/// ```
+/// #include <cz/heap.hpp>
+/// #include <cz/process.hpp>
+/// #include <cz/string.hpp>
+///
+/// struct Coroutine_Read_File_To_String : Coroutine {
+///     cz::Input_File file;
+///     cz::String string;
+///
+///     bool tick();
+/// };
+///
+/// bool Coroutine_Read_File_To_String::tick() {
+///     CZ_CO_START;
+///     while (1) {
+///         CZ_CO_YIELD(false);
+///
+///         char buffer[1024];
+///         int64_t result = file.read_binary(buffer, sizeof(buffer));
+///         if (result > 0) {
+///             // Process output.
+///             string.reserve(cz::heap_allocator(), result);
+///             string.append({buffer, (size_t)result});
+///         } else if (result == 0) {
+///             // Stop on end of file.
+///             break;
+///         } else {
+///             // Ignore errors.
+///         }
+///     }
+///     CZ_CO_END;
+///     file.close();
+///     return true;
+/// }
+///
+/// Coroutine_Read_File_To_String coroutine = {};
+/// if (coroutine.file.open("file_to_read")) {
+///     while (1) {
+///         if (coroutine.tick()) {
+///             break;
+///         }
+///     }
+///
+///     // Use the string.
+///     fputs("Read from file: ", stdout);
+///     fwrite(string.buffer(), 1, string.len(), stdout);
+///
+///     string.drop(cz::heap_allocator());
+/// }
+/// ```
+
 namespace cz {
 
 struct Coroutine {
