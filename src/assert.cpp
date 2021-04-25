94538eba8a78c02252007e6e0b65bab94cba60ae
Chris Gregory <czipperz@gmail.com>
2021-04-25 14:45:33 -0700

    Show TracyMessage on panic reached

--- src/assert.cpp
+++ src/assert.cpp
@@ -2,6 +2,12 @@
 
 #include <stdlib.h>
 
+#ifdef TRACY_ENABLE
+#include <stdio.h>
+#include <Tracy.hpp>
+#include <algorithm>
+#endif
+
 namespace cz {
 
 PanicReachedException::PanicReachedException(const char* message) : message(message) {}
@@ -13,6 +19,15 @@ const char* PanicReachedException::what() const noexcept {
 namespace impl {
 
 void panic_reached(const char* message) {
+#ifdef TRACY_ENABLE
+    ZoneScoped;
+    char buffer[1024];
+    int len = snprintf(buffer, sizeof(buffer), "Panic reached: %s", message);
+    len = std::min(len, (int)(sizeof(buffer) - 1));
+    CZ_ASSERT(len >= 0);
+    TracyMessage(buffer, len);
+#endif
+
     throw PanicReachedException(message);
 }
 
