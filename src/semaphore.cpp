b800a8f3d9a30eed3c2bc3e2d7180c981cd5066f
Chris Gregory <czipperz@gmail.com>
2022-06-23 15:40:16 -0700

    Add support for apple semaphores

--- src/semaphore.cpp
+++ src/semaphore.cpp
@@ -8,6 +8,8 @@
 #define WIN32_LEAN_AND_MEAN
 #define NOMINMAX
 #include <windows.h>
+#elif defined(__APPLE__)
+#include <dispatch/dispatch.h>
 #else
 #include <semaphore.h>
 #endif
@@ -23,6 +25,10 @@ static HANDLE h(void*& handle) {
     } else {
         return *(HANDLE*)handle;
     }
+}
+#elif defined(__APPLE__)
+static dispatch_semaphore_t* h(void* handle) {
+    return (dispatch_semaphore_t*)handle;
 }
 #else
 static sem_t* h(void* handle) {
@@ -45,11 +51,17 @@ void Semaphore::init(uint32_t initial_value) {
     } else {
         *(HANDLE*)handle = hh;
     }
+#elif defined(__APPLE__)
+    handle = cz::heap_allocator().alloc<dispatch_semaphore_t>();
+    CZ_ASSERT(handle);
+
+    *h(handle) = dispatch_semaphore_create(initial_value);
 #else
     handle = cz::heap_allocator().alloc<sem_t>();
     CZ_ASSERT(handle);
 
-    sem_init(h(handle), /*is_shared_between_processes=*/0, initial_value);
+    int result = sem_init(h(handle), /*is_shared_between_processes=*/0, initial_value);
+    CZ_ASSERT(result == 0);
 #endif
 }
 
@@ -59,6 +71,8 @@ void Semaphore::drop() {
     if (sizeof(HANDLE) > sizeof(void*)) {
         cz::heap_allocator().dealloc((HANDLE*)handle);
     }
+#elif defined(__APPLE__)
+    dispatch_release(*h(handle));
 #else
     sem_destroy(h(handle));
     cz::heap_allocator().dealloc(h(handle));
@@ -69,6 +83,8 @@ void Semaphore::release() {
 #ifdef _WIN32
     DWORD ret = ReleaseSemaphore(h(handle), /*increment=*/1, /*previous_count_out=*/nullptr);
     CZ_ASSERT(ret != 0);
+#elif defined(__APPLE__)
+    dispatch_semaphore_signal(*h(handle));
 #else
     int ret = sem_post(h(handle));
     CZ_ASSERT(ret == 0);
@@ -79,6 +95,9 @@ void Semaphore::acquire() {
 #ifdef _WIN32
     DWORD ret = WaitForSingleObject(h(handle), /*milli_seconds=*/INFINITE);
     CZ_ASSERT(ret == WAIT_OBJECT_0);
+#elif defined(__APPLE__)
+    intptr_t ret = dispatch_semaphore_wait(*h(handle), DISPATCH_TIME_FOREVER);
+    CZ_ASSERT(ret == 0);
 #else
     int ret = sem_wait(h(handle));
     CZ_ASSERT(ret == 0);
@@ -89,6 +108,9 @@ bool Semaphore::try_acquire() {
 #ifdef _WIN32
     // Note that this ignores errors.
     return WaitForSingleObject(h(handle), /*milli_seconds=*/0) == WAIT_OBJECT_0;
+#elif defined(__APPLE__)
+    // Note that this ignores errors.
+    return dispatch_semaphore_wait(*h(handle), DISPATCH_TIME_NOW) == 0;
 #else
     // Note that this ignores errors.
     return sem_trywait(h(handle)) == 0;
