14ecdc4848ff098482c9f1ecf7b93149321378ec
Chris Gregory <czipperz@gmail.com>
2022-06-23 15:45:50 -0700

    Parse overload for apple

--- include/cz/parse.hpp
+++ include/cz/parse.hpp
@@ -20,4 +20,10 @@ int64_t parse(cz::Str str, int16_t* out);
 int64_t parse(cz::Str str, int32_t* out);
 int64_t parse(cz::Str str, int64_t* out);
 
+#if __APPLE__ && __SIZEOF_SIZE_T__ == 8
+inline int64_t parse(cz::Str str, size_t* out) {
+    return parse(str, (uint64_t*)out);
+}
+#endif
+
 }
