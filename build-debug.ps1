ac829fc278a06a7ee63e901a604cfe4356ca23b5
Chris Gregory <czipperz@gmail.com>
2023-09-23 22:39:16 -0400

    fix color flag to use new catch2 flag

--- build-debug.ps1
+++ build-debug.ps1
@@ -4,7 +4,7 @@ try {
     ./run-build.ps1 build/debug Debug -DCZ_BUILD_TESTS=1
     if (!$?) { exit 1 }
 
-    ./build/debug/cz-test.exe --use-colour=no
+    ./build/debug/cz-test.exe --colour-mode=none
     if (!$?) { exit 1 }
 } finally {
     Pop-Location
