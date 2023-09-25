5b91eeec9f3ea9bcd2e538ed838ab5d6ba8bc150
Chris Gregory <czipperz@gmail.com>
2023-09-24 21:03:54 -0400

    Overload Catch2 printing of Str/String

--- test_base/czt/test_base.hpp
+++ test_base/czt/test_base.hpp
@@ -7,3 +7,14 @@
 #include <cz/std_string_format.hpp>
 
 #include "../catch.hpp"
+
+namespace Catch {
+template <>
+struct StringMaker<cz::String> {
+    static std::string convert(const cz::String& str);
+};
+template <>
+struct StringMaker<cz::Str> {
+    static std::string convert(const cz::Str& str);
+};
+}
--- test_base/main.cpp
+++ test_base/main.cpp
@@ -1,3 +1,20 @@
-#define CATCH_CONFIG_ENABLE_BENCHMARKING
 #define CATCH_CONFIG_MAIN
-#include "catch.hpp"
+#include "czt/test_base.hpp"
+
+#include <cz/string.hpp>
+
+namespace Catch {
+
+std::string StringMaker<cz::String>::convert(const cz::String& str) {
+    std::string std_string;
+    std_string.assign(str.buffer, str.len);
+    return std_string;
+}
+
+std::string StringMaker<cz::Str>::convert(const cz::Str& str) {
+    std::string std_string;
+    std_string.assign(str.buffer, str.len);
+    return std_string;
+}
+
+}
